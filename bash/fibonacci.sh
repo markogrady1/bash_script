1630d60d1303382f219a881f7318f0f1825dbf3b
markogrady1 <markogrady.code@gmail.com>
2015-07-17 21:18:08 +0100

    make array more efficient

--- bash/fibonacci.sh
+++ bash/fibonacci.sh
@@ -2,16 +2,12 @@
 
 #This script displays the fibonacci sequence in bash script
 
-fib=()
-fib[0]=0
-fib[1]=1
-fib[2]=1
-
+fib=(0 1 1)
 for i in {3..39}
 do
 	val=${fib[$i-1]}
 	val2=${fib[$i-2]}
-	fib[$i]=$(($val+$val2))
+	fib[$i]=$(( $val+$val2 ))
 done
 
 for j in {0..39}
