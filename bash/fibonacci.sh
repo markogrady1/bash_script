ea0b4b334ad5ce5616a38b53a6a14c8d3f33026a
markogrady1 <markogrady.code@gmail.com>
2015-07-17 21:10:42 +0100

    add different loop

--- bash/fibonacci.sh
+++ bash/fibonacci.sh
@@ -7,14 +7,14 @@ fib[0]=0
 fib[1]=1
 fib[2]=1
 
-for (( i=3; i<40; i++ ))
+for i in {3..39}
 do
 	val=${fib[$i-1]}
 	val2=${fib[$i-2]}
 	fib[$i]=$(($val+$val2))
 done
 
-for j in {0..40}
+for j in {0..39}
 do
 	echo ${fib[$j]}
 done
