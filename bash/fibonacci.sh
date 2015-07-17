0e0d2ed76a3971dce5b12fd237d560f6166ed90c
markogrady1 <markogrady.code@gmail.com>
2015-07-17 21:06:17 +0100

    fix typo with the val variables

--- bash/fibonacci.sh
+++ bash/fibonacci.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
+
+#This script displays the fibonacci sequence in bash script
+
 fib=()
 fib[0]=0
 fib[1]=1
 fib[2]=1
+
 for (( i=3; i<40; i++ ))
 do
 	val=${fib[$i-1]}
 	val2=${fib[$i-2]}
-	fib[$i]=$((val+val2))
-#	echo ${fib[$i]}
+	fib[$i]=$(($val+$val2))
 done
 
 for j in {0..40}
